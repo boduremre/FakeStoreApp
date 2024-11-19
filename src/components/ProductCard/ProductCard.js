13d5abc40951e93fb89dfb6671c6b566b8ff0bad
Emre Bodur <emrebodurbto@gmail.com>
2024-11-20 00:47:47 +0300

    Update ProductCard.js

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Card } from "antd";
-import { EditOutlined, SettingOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import "./ProductCard.css";
 
@@ -11,22 +10,17 @@ const ProductCard = ({ product }) => {
     <Link to={`/product/${product.id}`}>
       <Card
         cover={<img alt={product.title} src={product.image} />}
-        actions={[
-          <SettingOutlined key="setting" />,
-          <EditOutlined key="edit" />,
-        ]}
-        title={product.title}
         hoverable
         style={{
-          width: 200,
-          height: 200,
+          width: 240,
+          height: "auto",
           marginLeft: "5px",
           marginTop: "10px",
           display: "flex",
           flexDirection: "column",
         }} // Sabit genişlik ve yükseklik
       >
-        <Meta title={product.title} description={`$${product.price}`} />
+        <Meta title={product.title} description={`${product.price}₺`} />
       </Card>
     </Link>
   );
